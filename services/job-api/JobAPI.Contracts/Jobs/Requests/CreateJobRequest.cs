c53406c9d7095a389253d87204be5fcdf7edef78
Elkhair <eelkhair@lunavi.com>
2025-07-21 01:17:17 -0500

    cleanup

--- services/job-api/JobAPI.Contracts/Jobs/Requests/CreateJobRequest.cs
+++ services/job-api/JobAPI.Contracts/Jobs/Requests/CreateJobRequest.cs
@@ -1,15 +1,16 @@
 ﻿namespace JobAPI.Contracts.Jobs.Requests;
 
-public record CreateJobRequest(
-    string Title,
-    string Company,
-    string Location,
-    string JobType,
-    string SalaryRange,
-    string PostedAt,
-    string AboutCompany,
-    string AboutRole,
-    List<string> Responsibilities,
-    List<string> Qualifications,
-    string EEO
-);
+public class CreateJobRequest 
+{
+    public required string Title { get; set; }
+    public required string Company { get; set; }
+    public required string Location { get; set; }
+    public required string JobType { get; set; }
+    public required string AboutRole { get; set; }
+    public string? SalaryRange { get; set; }
+    public string? PostedAt { get; set; }
+    public string? AboutCompany { get; set; }
+    public string? EEO { get; set; }  
+    public List<string> Responsibilities { get; set; } = [];
+    public List<string> Qualifications { get; set; } = [];
+}
