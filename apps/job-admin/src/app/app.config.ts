9f2ef00095601328f3a7a6d6c8751952a2143a6b
Elkhair <eelkhair@lunavi.com>
2025-07-26 08:26:22 -0500

    angular auth0 setup

--- apps/job-admin/src/app/app.config.ts
+++ apps/job-admin/src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {provideAuth0} from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'elkhair-dev.us.auth0.com',           // e.g., dev-abc123.us.auth0.com
+      clientId: 'YXnqj0gOfZJD8Ypje7mdZqdoenCHNzWA',      // from Auth0 dashboard
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      }
+    }),
   ]
 };
--- apps/job-admin/src/app/app.ts
+++ apps/job-admin/src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {AuthButtonComponent} from './AuthButton';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('job-admin');
+  protected http = inject(HttpClient);
+
+  ngOnInit() {
+    this.http.get('https://job-api.eelkhair.net/companies').subscribe({
+      next: data => { console.log(data); },
+    })
+  }
 }
