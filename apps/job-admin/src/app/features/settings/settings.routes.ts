57bd17de2f1aa7756ce6102c422a6a64715f388e
Elkhair <eelkhair@lunavi.com>
2025-12-18 22:41:16 -0600

    settings

--- apps/job-admin/src/app/features/settings/settings.routes.ts
+++ apps/job-admin/src/app/features/settings/settings.routes.ts
@@ -1,3 +1,11 @@
 import {Route} from '@angular/router';
+import {roleGuard} from '../../core/guards/role-guard';
 
-export const SETTINGS_ROUTES: Route[] = []
+export const SETTINGS_ROUTES: Route[] = [
+  {
+    path:'app',
+    canActivate: [roleGuard],
+    data: {roles: ['admin']},
+    loadComponent: () => import('./settings').then(c=>c.Settings)
+  }
+]
